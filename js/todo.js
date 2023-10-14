59af1d72d520773c19efeb459647b69e58bf7877
wjdalswl <109158284+wjdalswl@users.noreply.github.com>
2023-10-14 23:29:28 +0900

    complete

--- js/todo.js
+++ js/todo.js
@@ -23,7 +23,8 @@ function paintToDo(newTodo) {
     const span = document.createElement("span");
     span.innerText = newTodo.text;
     const button = document.createElement("button");
-    button.innerText = "❌";
+    button.classList.add("delete-button");
+    button.innerText = "X";
     button.addEventListener("click", deleteToDo);
     li.appendChild(span);
     li.appendChild(button);
